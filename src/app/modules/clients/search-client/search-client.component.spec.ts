ed5454ee2ed226d11a1462422584d81fbdeeec2b
Christian Cano <christian.cano@fouriering.com>
2024-09-20 15:41:39 -0500

    Revert "jest configuration"
    This reverts commit 9cf63198d8319e9bd355ab699fef6fd17f312899.

--- src/app/modules/clients/search-client/search-client.component.spec.ts
+++ src/app/modules/clients/search-client/search-client.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { of } from 'rxjs';
-import { clientMock } from './../../../../test/mocks/client.mock';
-import { clientsServiceMock } from './../../../../test/services/clients.service.mock';
+import { Client } from './../../../shared/models/client.model';
 import { ClientsService } from './../../../shared/services/clients.service';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { SearchClientComponent } from './search-client.component';
+
+// describe('SearchClientComponent', () => {
+//   let component: SearchClientComponent;
+//   let fixture: ComponentFixture<SearchClientComponent>;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [SearchClientComponent]
+//     });
+//     fixture = TestBed.createComponent(SearchClientComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchClientComponent } from './search-client.component';
+// import { ClientsService } from '../clients.service'
+import { of } from 'rxjs';
 
 describe('SearchClientComponent', () => {
   let component: SearchClientComponent;
   let fixture: ComponentFixture<SearchClientComponent>;
+  let clientsServiceSpy: jasmine.SpyObj<ClientsService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('ClientsService', [
+      'searchClientBySharedKey',
+    ]);
+
     TestBed.configureTestingModule({
       declarations: [SearchClientComponent],
-      imports: [ReactiveFormsModule, FormsModule],
-      providers: [{ provide: ClientsService, useValue: clientsServiceMock }],
+      providers: [{ provide: ClientsService, useValue: spy }],
     });
 
     fixture = TestBed.createComponent(SearchClientComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    clientsServiceSpy = TestBed.inject(
+      ClientsService
+    ) as jasmine.SpyObj<ClientsService>;
   });
 
   it('should create', () => {
@@ -27,49 +53,48 @@ describe('SearchClientComponent', () => {
   });
 
   it('should emit found clients when searchClientBySharedKey is called', () => {
-    jest.spyOn<any, string>(component.foundClients, 'emit');
+    const mockClients: Client[] = [
+      {
+        sharedKey: 'cariza',
+        name: 'Carlos Ariza',
+        email: 'cariza@gmail.com',
+        phone: '12345678',
+        dataAdded: '20/05/2024',
+      },
+    ];
+    clientsServiceSpy.searchClientBySharedKey.and.returnValue(of(mockClients));
+
+    spyOn(component.foundClients, 'emit');
     component.searchTerm = 'test';
     component.searchClientBySharedKey();
 
-    expect(
-      component['_clientsService'].searchClientBySharedKey
-    ).toHaveBeenCalledWith('test');
-    expect(component.foundClients.emit).toHaveBeenCalledWith([clientMock]);
+    expect(clientsServiceSpy.searchClientBySharedKey).toHaveBeenCalledWith(
+      'test'
+    );
+    expect(component.foundClients.emit).toHaveBeenCalledWith(mockClients);
   });
 
-  it('should not emit when no clients are found', () => {
-    jest
-      .spyOn<any, string>(
-        component['_clientsService'],
-        'searchClientBySharedKey'
-      )
-      .mockReturnValue(of([]));
+  // it('should not emit when no clients are found', () => {
+  //   clientsServiceSpy.searchClientBySharedKey.and.returnValue(of([]));
 
-    jest.spyOn(component.foundClients, 'emit');
-    component.searchTerm = 'nonexistent';
-    component.searchClientBySharedKey();
+  //   spyOn(component.foundClients, 'emit');
+  //   component.searchTerm = 'nonexistent';
+  //   component.searchClientBySharedKey();
 
-    expect(
-      component['_clientsService'].searchClientBySharedKey
-    ).toHaveBeenCalledWith('nonexistent');
-    expect(component.foundClients.emit).toHaveBeenCalledWith([]);
-  });
+  //   expect(clientsServiceSpy.searchClientBySharedKey).toHaveBeenCalledWith(
+  //     'nonexistent'
+  //   );
+  //   expect(component.foundClients.emit).toHaveBeenCalledWith([]);
+  // });
 
-  it('should handle empty search term', () => {
-    jest
-      .spyOn<any, string>(
-        component['_clientsService'],
-        'searchClientBySharedKey'
-      )
-      .mockReturnValue(of([]));
+  // it('should handle empty search term', () => {
+  //   clientsServiceSpy.searchClientBySharedKey.and.returnValue(of([]));
 
-    jest.spyOn(component.foundClients, 'emit');
-    component.searchTerm = '';
-    component.searchClientBySharedKey();
+  //   spyOn(component.foundClients, 'emit');
+  //   component.searchTerm = '';
+  //   component.searchClientBySharedKey();
 
-    expect(
-      component['_clientsService'].searchClientBySharedKey
-    ).toHaveBeenCalledWith('');
-    expect(component.foundClients.emit).toHaveBeenCalledWith([]);
-  });
+  //   expect(clientsServiceSpy.searchClientBySharedKey).toHaveBeenCalledWith('');
+  //   expect(component.foundClients.emit).toHaveBeenCalledWith([]);
+  // });
 });
--- src/test/mocks/client.mock.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from 'src/app/shared/models/client.model';
-
-export const clientMock: Client = {
-  sharedKey: 'cariza',
-  name: 'Carlos Ariza',
-  email: 'cariza@gmail.com',
-  phone: '12345678',
-  dataAdded: '20/05/2024',
-};
